5dc433426ca8d72500718647c53bd8071b22afbb
Adnan Dev X <adnandevx@gmail.com>
2026-03-22 20:49:37 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React,{useEffect, useState, useRef} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
+
+
+
+function Main(){
+var [data,setdata] = useState([
+])
+
+var [ip,sip] = useState("")
+
+  return (
+    <>
+   <div className='container'>
+    <h2>Todo Applications</h2>
+    <div className='input-container'>
+    <input onChange={(c) => {
+      sip(c.target.value)
+    }} />
+    <button onClick={() => {
+      setdata([...data, {id: data.length + 1, job: ip}])
+
+    }}>Add</button>
+    </div>
+
+    <ul>
+
+      {
+       data.map((x) => 
+      
+    <div className='todo-item'>
+    <li key={x.id}>{x.id}.{x.job}</li>
+    <button onClick={() => {
+      setdata(data.filter(item => item.id !== x.id ))
+    }} >Delete</button>
+   </div>
+      )
+      }
+
+    </ul>
+    
+   </div>
+    </>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Main/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
